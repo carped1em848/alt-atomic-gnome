c797d69ac2a6dbf261d9a8e0f9cbe29208e553bd
dm <dmitry@udalov.online>
2025-01-24 20:13:05 +0600

    test github workflows

--- src/main.sh
+++ src/main.sh
@@ -7,23 +7,23 @@ set -e
 echo "Running main.sh..."
 
 # Базовые пакеты для работы системы
-./packages/apt_prepare.sh
-./packages/base.sh
-./packages/DE/GNOME/gnome.sh
-./packages/apt_ending.sh
+#./packages/apt_prepare.sh
+#./packages/base.sh
+#./packages/DE/GNOME/gnome.sh
+#./packages/apt_ending.sh
 
 # Настройка
-./configuration/branding.sh
-./configuration/settings.sh
-./configuration/user.sh
-./configuration/kernel.sh
-./make/zstd.sh
-./make/cargo.sh
-./make/bootupd.sh
-./make/bootc.sh
-./make/brew.sh
-./make/zsh-plugins.sh
-./make/atomic-actions.sh
-./configuration/clear.sh
+#./configuration/branding.sh
+#./configuration/settings.sh
+#./configuration/user.sh
+#./configuration/kernel.sh
+#./make/zstd.sh
+#./make/cargo.sh
+#./make/bootupd.sh
+#./make/bootc.sh
+#./make/brew.sh
+#./make/zsh-plugins.sh
+#./make/atomic-actions.sh
+#./configuration/clear.sh
 
 echo "All scripts executed successfully."
