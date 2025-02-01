5ef487854d1c15b537cc277c4e5fd68fa141b89a
dm <dmitry@udalov.online>
2025-02-02 03:18:28 +0600

    fix: branding and nvidia packages

--- src/configuration/branding.sh
+++ src/configuration/branding.sh
@@ -8,12 +8,40 @@ cat << EOF > /etc/os-release
 NAME="ALT Atomic"
 VERSION="0.1"
 VERSION_ID="0.1"
+PRETTY_NAME="ALT Atomic"
 ID=altlinux
 ID_LIKE="altlinux"
 PLATFORM_ID="platform:altlinux"
 PRETTY_NAME="ALT Atomic"
 VARIANT="Atomic"
 VARIANT_ID="atomic"
+ANSI_COLOR="1;33"
+CPE_NAME="cpe:/o:alt:sisyphus:20240122"
+BUILD_ID="Sisyphus 20240122"
+ALT_BRANCH_ID="sisyphus"
+HOME_URL="https://atomic.alt-gnome.ru/"
+BUG_REPORT_URL="https://atomic.alt-gnome.ru/"
+LOGO=altlinux
+EOF
+
+cat << EOF > /usr/lib/os-release
+NAME="ALT Atomic"
+VERSION="0.1"
+VERSION_ID="0.1"
+PRETTY_NAME="ALT Atomic"
+ID=altlinux
+ID_LIKE="altlinux"
+PLATFORM_ID="platform:altlinux"
+PRETTY_NAME="ALT Atomic"
+VARIANT="Atomic"
+VARIANT_ID="atomic"
+ANSI_COLOR="1;33"
+CPE_NAME="cpe:/o:alt:sisyphus:20240122"
+BUILD_ID="Sisyphus 20240122"
+ALT_BRANCH_ID="sisyphus"
+HOME_URL="https://atomic.alt-gnome.ru/"
+BUG_REPORT_URL="https://atomic.alt-gnome.ru/"
+LOGO=altlinux
 EOF
 
 echo "End branding.sh"
--- src/packages/components/nvidia/packages.sh
+++ src/packages/components/nvidia/packages.sh
@@ -4,6 +4,6 @@ echo "Installing nvidia packages"
 
 apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
 apt-get update && \
-apt-get install -y kernel-modules-nvidia-open-6.12 libnvidia-opencl libnvoptix cuda-libs
+apt-get install -y kernel-modules-nvidia-6.12 libnvidia-opencl libnvoptix cuda-libs opencl-headers opencl-cpp-headers nvidia-cuda-toolkit nvidia-cuda-devel
 
 echo "End installing nvidia packages"
