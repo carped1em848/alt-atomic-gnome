7c57fca06f298644a1c01f4204cba0e4af630dc9
dm <dmitry@udalov.online>
2025-02-04 19:08:38 +0600

    fix: add atomic actions for upgrade build and change nvidia packages

--- src/packages/components/nvidia/packages.sh
+++ src/packages/components/nvidia/packages.sh
@@ -6,6 +6,6 @@ echo "Installing nvidia packages"
 
 apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
 apt-get update && \
-apt-get install -y kernel-modules-nvidia-open-6.12 libnvidia-opencl libnvoptix cuda-libs opencl-headers opencl-cpp-headers nvidia-cuda-toolkit nvidia-cuda-devel
+apt-get install -y kernel-modules-nvidia-open-6.12 libnvidia-opencl libnvoptix cuda-libs
 
 echo "End installing nvidia packages"
