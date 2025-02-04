5091afec166764c20694087d38d17fc5f571dc9c
dm <dmitry@udalov.online>
2025-02-04 22:46:39 +0600

    fix: change nvidia build

--- src/packages/components/nvidia/packages.sh
+++ src/packages/components/nvidia/packages.sh
@@ -6,6 +6,6 @@ echo "Installing nvidia packages"
 
 apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
 apt-get update && \
-apt-get install -y kernel-modules-nvidia-open-6.12 libnvidia-opencl libnvoptix cuda-libs
+apt-get install -y kernel-modules-nvidia-open-6.12 nvidia_glx libnvoptix
 
 echo "End installing nvidia packages"
