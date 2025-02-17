1e842d990215b765b4127ae49c274aba1f355605
dm <dmitry@udalov.online>
2025-02-17 16:55:13 +0600

    fix: change nvidia packages

--- src/packages/02-nvidia-packages.sh
+++ src/packages/02-nvidia-packages.sh
@@ -7,7 +7,7 @@ echo "::group:: ===$(basename "$0")==="
 if [ "$BUILD_TYPE" = "nvidia" ]; then
   apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
   apt-get update && \
-  apt-get install -y kernel-modules-nvidia-open-6.12 nvidia_glx libnvoptix
+  apt-get install -y kernel-modules-nvidia-open-6.12 nvidia_glx libnvoptix nvidia-tools libnvidia-ml
 fi
 
 echo "::endgroup::"
