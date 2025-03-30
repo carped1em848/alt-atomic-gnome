a484f373f4dc84cc00c536f9a5d927f03256ac8c
dm <dmitry@udalov.online>
2025-03-30 16:23:10 +0600

    flx: test another install nvidia driver

--- src/packages/02-nvidia-packages.sh
+++ src/packages/02-nvidia-packages.sh
@@ -5,14 +5,9 @@ set -e
 echo "::group:: ===$(basename "$0")==="
 
 if [ "$BUILD_TYPE" = "nvidia" ]; then
-  apt-get install nvidia_glx_common && \
-  nvidia-install-driver
-
-#  apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
-#  apt-get update && \
-#  apt-get install -y kernel-modules-nvidia-open-6.12 nvidia_glx libnvoptix nvidia-tools libnvidia-ml
-
-  echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nvidia-x11.conf
+  apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
+  apt-get update && \
+  apt-get install -y kernel-modules-nvidia-open-6.12 nvidia_glx libnvoptix nvidia-tools libnvidia-ml
 fi
 
 echo "::endgroup::"
